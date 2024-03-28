d2555b97e528d4ebeed718984f006ecde0d70eb7
yandri918 <150146344+yandri918@users.noreply.github.com>
2024-03-28 21:02:23 +0900

    Add files via upload

--- latihan.py
+++ latihan.py
@@ -23,3 +23,36 @@ nama = "andriyanto"
 print(f"hello nama saya {nama}")
 print ("nama saya {}".format(nama))
 print("nama saya %s"%(nama))
+a =[1,2,"belajar"]
+print(type(a))
+print(a[2])
+a[0]= "kemarin"
+print (a)
+d= (1, "andriyanto",2)
+print(type(d))
+print(d[0:2])
+print(d[2])
+e={1,2,3,4,5,6,7}
+print(type(e))
+print(e)
+set1= {1,2,4,5,7,8,6}
+set2={5,6,7,9,}
+union = set1.union(set2)
+print("union:",union)
+intersection=set1.intersection(set2)
+print("intersection:",intersection)
+z= {'nama':'andriyanto','umur':35,'status':'menikah','pekerjaan':'petani'}
+print(type(z))
+
+print(z['status'])
+z['hobi']= 'programming'
+print(z)
+z ['nama']='rina'
+print(z)
+print(float(12))
+print(int(2.3))
+print(set([1,2,3]))
+print(tuple({1,3,5}))
+print(list('andriyanto'))
+print(dict[[1,2],[3,4]])
+print(dict([(2,3),(3.4,3)]))

f0fcb2ac15d7ef4e4710a5cbec7f021bbde428a1
yandri918 <150146344+yandri918@users.noreply.github.com>
2024-03-28 20:15:40 +0900

    Add files via upload

---
+++ latihan.py
@@ -0,0 +1,25 @@
+print("hello world")
+a = 7
+b = 3
+c = a+b
+print(c)
+age = 23
+salary = "1500000"
+print(type(age))
+print(type(salary))
+var = 10
+print(var)
+print(id(var))
+var = 11
+print(var)
+print(id(var))
+x = True
+print(type(x))
+b = "dicoding.com"
+print(type(b))
+print(b[2])
+print(b[2:])
+nama = "andriyanto"
+print(f"hello nama saya {nama}")
+print ("nama saya {}".format(nama))
+print("nama saya %s"%(nama))
